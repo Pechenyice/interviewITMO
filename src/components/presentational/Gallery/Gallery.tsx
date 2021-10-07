c79ccd43998d41d578b2d5a92d860896a9620abc
pechenyice <german.iskhakov@mail.ru>
2021-10-07 20:33:32 +0300

    started gallery component layout and styles

---
+++ src/components/presentational/Gallery/Gallery.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Gallery.module.css';
+
+const Gallery = () => {
+    return (
+        <section className={styles.galleryWrapper}>
+
+        </section>
+    );
+}
+
+export default Gallery;
